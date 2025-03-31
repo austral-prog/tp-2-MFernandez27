8a4edae678ceb0b2d491771c0f529f915f441611
MFernandez27 <mfernandez27@mail.austral.edu.ar>
2025-03-31 18:06:01 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,8 @@
 def change():
     expense = 23.75
     money = 100
+    full_name = f"{first_name} {last_name}"
+    print(full_name.lower())
+    print(full_name.title())
+    print(full_name.upper())
+    print(f"\t{full_name.lower()}") 
